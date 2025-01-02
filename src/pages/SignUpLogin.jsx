d44b003722759041622ff137e5af441bc667aaef
dgalilove <dgalilove@gmail.com>
2025-01-02 15:29:35 +0200

    Login style

--- src/pages/SignUpLogin.jsx
+++ src/pages/SignUpLogin.jsx
@@ -3,7 +3,20 @@ import React from 'react'
 export function SignUpLogin() {
   return (
     <div className="signup-login">
-      
+      <div className='login'>
+        <img src="src/assets/img/logo.PNG" alt="" />
+        <input type='email' name='email' placeholder='Enter your email'></input>
+        <input type='password' name='password' placeholder='Enter your password'></input>
+        <button>Login</button>
+      </div>
+
+
+      <div className='options'>
+        <p> ------ Or ------</p>
+        <p>Forgot Your Password?</p>
+        <p>Don't have an account? <span>Sign Up</span></p>
+      </div>
+
     </div>
   )
 }
